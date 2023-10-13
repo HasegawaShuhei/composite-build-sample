e528c0c1077febe97125e818b7bc4e64866f496e
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-13 22:51:07 +0900

    不要な記述削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.google.fonts)
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
 
