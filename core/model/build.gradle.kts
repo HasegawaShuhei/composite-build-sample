cd7f2d9cded5d5a0387f40e2f550b9876c0f04c3
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-14 23:32:05 +0900

    core/modelモジュール追加

---
+++ core/model/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("compositebuildsample.android.library")
+    id("compositebuildsample.android.library.compose")
+}
+
+android {
+    namespace = "com.example.compositebuildsample.core.model"
+}
+
+dependencies {
+
+}
