13f546c14f0ab08e277650f2bc0576ca2e409f3d
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-15 23:03:54 +0900

    オブジェクト名修正

--- core/database/src/main/java/com/example/compositebuildsample/core/database/dao/MemoDao.kt
+++ core/database/src/main/java/com/example/compositebuildsample/core/database/dao/MemoDao.kt
@@ -5,17 +5,17 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.compositebuildsample.core.database.model.Memo
+import com.example.compositebuildsample.core.database.model.MemoEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MemoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsert(memo: Memo)
+    suspend fun upsert(memo: MemoEntity)
 
     @Delete
-    suspend fun delete(memo: Memo)
+    suspend fun delete(memo: MemoEntity)
 
     @Query("SELECT * FROM memos")
-    fun getMemos(): Flow<List<Memo>>
+    fun getMemos(): Flow<List<MemoEntity>>
 }
--- core/database/src/main/java/com/example/compositebuildsample/core/database/model/MemoEntity.kt
+++ core/database/src/main/java/com/example/compositebuildsample/core/database/model/MemoEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "memos")
-data class Memo(
+data class MemoEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val description: String,
