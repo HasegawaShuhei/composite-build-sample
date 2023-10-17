d46719ed7396bddf0a1df025a0a61f5d920566f6
HasegawaShuhei <sh1027.dev@gmail.com>
2023-10-17 23:04:57 +0900

    MemoRepositoryModuleの記述方法変更

--- core/data/src/main/java/com/example/compositebuildsample/core/data/di/MemoRepositoryModule.kt
+++ core/data/src/main/java/com/example/compositebuildsample/core/data/di/MemoRepositoryModule.kt
@@ -1,21 +1,27 @@
 package com.example.compositebuildsample.core.data.di
 
 import com.example.compositebuildsample.core.data.repository.MemoRepositoryImpl
-import com.example.compositebuildsample.core.database.dao.MemoDao
 import com.example.compositebuildsample.core.domain.repository.MemoRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object MemoRepositoryModule {
+interface MemoRepositoryModule {
 
-    @Provides
-    @Singleton
-    fun provideMemoRepository(memoDao: MemoDao): MemoRepository {
-        return MemoRepositoryImpl(memoDao)
-    }
+    @Binds
+    fun bindsMemoRepository(memoRepository: MemoRepositoryImpl): MemoRepository
 }
+
+//@Module
+//@InstallIn(SingletonComponent::class)
+//object MemoRepositoryModule {
+//
+//    @Provides
+//    @Singleton
+//    fun provideMemoRepository(memoDao: MemoDao): MemoRepository {
+//        return MemoRepositoryImpl(memoDao)
+//    }
+//}
